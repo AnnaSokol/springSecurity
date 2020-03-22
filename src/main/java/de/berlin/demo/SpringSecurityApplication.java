f64997beccbadfaeb24918bdb2d586ab3941c06f
ANNAFIW <9.3.1934Gagarin>
2020-03-22 21:11:12 +0100

    Spring security service

---
+++ src/main/java/de/berlin/demo/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package de.berlin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
